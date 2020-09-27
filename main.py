5ce51b94ec832c03bdaeb4750bc883bc4297fd8d
Sergiorq1 <66581064+Sergiorq1@users.noreply.github.com>
2020-09-27 14:16:51 -0700

    Added function names

---
+++ main.py
@@ -0,0 +1,6 @@
+def get_file_lines(filename):
+    #return a list of strings containing the lines of the file.
+
+def lines_printed_backwards(lines_list):
+    #print the lines of the poem in reverse. Include the original line number at the beginning of each line.
+    
